8ec19f487fcd1777c30c34ca66744a2fc748715c
PAULO DE FREITAS MARSIGLIA JUNIOR <paulo.marsiglia@edu.pucrs.br>
2025-03-26 09:19:12 -0300

    commit inicial

---
+++ src/ContaBancaria.java
@@ -0,0 +1,43 @@
+public class ContaBancaria{
+    int numeroConta;
+    String nomeCorrentista;
+    double saldo;
+
+    public ContaBancaria(int numeroConta, String nomeCorrentista){
+        this.numeroConta = numeroConta;
+        this.nomeCorrentista = nomeCorrentista;
+        this.saldo = 0;
+    }
+
+    public int getNroConta(){
+        return numeroConta;
+    }
+
+    public String getNomeCorrentista(){
+        return nomeCorrentista;
+    }
+
+    public double getSaldo(){
+        return saldo;
+    }
+
+    public boolean deposito(double valor){
+        if(valor > 0){
+            saldo += valor;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public boolean retirada(double valor){
+        if(valor >= saldo){
+            saldo -= valor;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
